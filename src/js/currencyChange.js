6f2dc06f36df5ce8e1ec2e022805087efa7442d4
奈々 <yandcheng@gmail.com>
2025-04-17 09:54:37 +0800

    curency exchage simplefy

--- src/js/currencyChange.js
+++ src/js/currencyChange.js
@@ -1,9 +1,28 @@
 import { useSelector } from "react-redux";
 import { selectCurrency } from "../redux/currencySlice";
 
+var exchange = {
+    'JPY':{
+        'USD':0.007,
+        'JPY':1,
+        'TWD':0.23
+    },
+}
+
 export default function currencyChange(currency, price) {
     const targetCurrency = useSelector(selectCurrency);
-    switch (currency) {
+    var result = 0;
+    try {
+        if (currency == targetCurrency) {
+            result = price;
+        } else {
+            result = price * exchange[currency][targetCurrency];
+        }
+    } catch (error) {
+        result = price * (1/exchange[targetCurrency][currency]);
+    }
+    return(result);
+    /*switch (currency) {
         case "TWD":
             if(targetCurrency == "TWD"){
                 return(price)
@@ -35,5 +54,5 @@ export default function currencyChange(currency, price) {
             if (targetCurrency == "JPY") {
                 return (price * 143.01);
             }
-    }
+    }*/
 }
