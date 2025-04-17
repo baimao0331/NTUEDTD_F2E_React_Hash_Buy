ee0603f569a555148f92a074f443fb2ea1c16ebd
奈々 <yandcheng@gmail.com>
2025-04-17 21:32:46 +0800

    dim

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router'
 import currencyChange from '../js/currencyChange'
 import { useSelector } from "react-redux";
 import { selectCurrency } from "../redux/currencySlice";
-import { flip, shift, useFloating,useHover, autoUpdate, useInteractions, safePolygon} from '@floating-ui/react';
+import { flip, shift, useFloating,useHover, autoUpdate, useInteractions, safePolygon, FloatingOverlay} from '@floating-ui/react';
 import { useState } from 'react';
 
 export default function Listitem(props) {
@@ -25,7 +25,25 @@ export default function Listitem(props) {
 
     return (
         <>
-            
+
+            {isOpen && (
+                <FloatingOverlay>
+                <div ref={refs.setFloating} style={floatingStyles} className="max-w-sm rounded overflow-hidden shadow-lg" {...getFloatingProps()}>
+                    
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+                </div>
+            </FloatingOverlay>
+            )}
             <div key={id} ref={refs.setReference} className="  h-25 w-full flex pl-4 pr-6 relative group text-stone-900 dark:text-stone-50"{...getReferenceProps()}>
                 <Link to={`/products/id/` + id} className=' w-full h-full flex'>
                     <figure className=" h-2/3 w-1/4 aspect-[2/1] overflow-hidden rounded-sm self-center">
@@ -41,22 +59,7 @@ export default function Listitem(props) {
                     </div>
                 </Link>
             </div>
-            {isOpen && (
-                <div ref={refs.setFloating} style={floatingStyles} className="max-w-sm rounded overflow-hidden shadow-lg" {...getFloatingProps()}>
-                    
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                        <p className="text-gray-700 text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                        </p>
-                    </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-            </div>
-            )}
+ 
         </>
     )
 }
