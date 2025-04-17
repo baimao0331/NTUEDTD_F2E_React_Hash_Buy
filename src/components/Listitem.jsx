980f19a452f909bde91853d571fdd6571aee6af2
奈々 <yandcheng@gmail.com>
2025-04-17 21:10:31 +0800

    help

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -2,15 +2,29 @@ import { Link } from 'react-router'
 import currencyChange from '../js/currencyChange'
 import { useSelector } from "react-redux";
 import { selectCurrency } from "../redux/currencySlice";
+import { flip, shift, useFloating,useHover, autoUpdate} from '@floating-ui/react';
+import { useState } from 'react';
 
 export default function Listitem(props) {
     const { id, title, image, price, discount, currency } = props;
     const targetCurrency = useSelector(selectCurrency);
-    
+    const [isOpen, setIsOpen] = useState(false);
+    const {refs, floatingStyles, context} = useFloating({
+        whileElementsMounted: autoUpdate,
+        open: isOpen,
+        onOpenChange: setIsOpen,
+        placement: 'right-start',
+        middleware:[flip({crossAxis:false,}),shift({crossAxis:false,})]
+    });
+    const hover = useHover(context);
+    const {getReferenceProps, getFloatingProps} = useInteractions([
+        hover,
+    ]);
+
     return (
         <>
             
-            <div key={id} className="  h-25 w-full flex pl-4 pr-6 relative group text-stone-900 dark:text-stone-50">
+            <div key={id} ref={refs.setReference} className="  h-25 w-full flex pl-4 pr-6 relative group text-stone-900 dark:text-stone-50"{...getReferenceProps()}>
                 <Link to={`/products/id/` + id} className=' w-full h-full flex'>
                     <figure className=" h-2/3 w-1/4 aspect-[2/1] overflow-hidden rounded-sm self-center">
                         <img src={`/images/` + id + `/` + image} alt="商品圖" className=" h-full w-full object-cover object-center" />
@@ -25,6 +39,22 @@ export default function Listitem(props) {
                     </div>
                 </Link>
             </div>
+            {isOpen && (
+                <div ref={refs.setFloating} style={floatingStyles} class="max-w-sm rounded overflow-hidden shadow-lg" {...getFloatingProps()}>
+                    
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            )}
         </>
     )
 }
