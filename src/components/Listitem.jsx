86ba7199f17a02955380b7487f77b8b6fff850dd
Baimao <shunliu94331@gmail.com>
2025-04-17 21:16:14 +0800

    import useInteractions

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router'
 import currencyChange from '../js/currencyChange'
 import { useSelector } from "react-redux";
 import { selectCurrency } from "../redux/currencySlice";
-import { flip, shift, useFloating,useHover, autoUpdate} from '@floating-ui/react';
+import { flip, shift, useFloating,useHover, autoUpdate, useInteractions} from '@floating-ui/react';
 import { useState } from 'react';
 
 export default function Listitem(props) {
@@ -40,18 +40,18 @@ export default function Listitem(props) {
                 </Link>
             </div>
             {isOpen && (
-                <div ref={refs.setFloating} style={floatingStyles} class="max-w-sm rounded overflow-hidden shadow-lg" {...getFloatingProps()}>
+                <div ref={refs.setFloating} style={floatingStyles} className="max-w-sm rounded overflow-hidden shadow-lg" {...getFloatingProps()}>
                     
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                        <p class="text-gray-700 text-base">
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <p className="text-gray-700 text-base">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                         </p>
                     </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
                 </div>
             </div>
             )}
